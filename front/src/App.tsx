cd7381d9f10cbf992b735de74de81db767ebedcd
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2023-10-01 02:21:23 +0200

    [DEVOPS]: test automatic deploiement

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test automatic deploiement
+          Learn React hello test
         </a>
       </header>
     </div>
